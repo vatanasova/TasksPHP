41106b022db91098c57bf942c1d6bd822193efc6
vatanasova <victoria_a@gbg.bg>
2015-09-07 16:04:53 +0300

    HTML and CSS modified

--- resources/views/tasks/default.blade.php
+++ resources/views/tasks/default.blade.php
@@ -4,10 +4,9 @@
         <title>{{ $title }}</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/task.css">
         <script src="js/jquery-2.1.4.js"></script>
     </head>
-    <body class="base">
+    <body>
         @yield('content')
     </body>
 </html>
--- resources/views/tasks/task.blade.php
+++ resources/views/tasks/task.blade.php
@@ -1,17 +1,24 @@
 @extends('tasks.default')
 
 @section('content')
+<link rel="stylesheet" type="text/css" href="css/task.css">
 <script src="js/task.js"></script>
-        <form method="post" id="form">
-            <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-            <p>Select function to perform over the string</p>
-            <input type="radio" name="func" value="reverse" checked>Reverse string<br>
-            <input type="radio" name="func" value="palindrome">Check if the string is palindrome<br>
-            <input type="radio" name="func" value="encrypt">Encrypt string<br>
-            <input type="radio" name="func" value="decrypt">Decrypt string<br><br><br>
-            Input string:&nbsp;
-            <input type="text" name="value" required>
-            <input type="submit" value="Execute">
-            <p id="result"></p>
-        </form>
+<div class="base">
+    <form method="post" id="form">
+        <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+        
+        <p>Select function to perform over the string</p>
+        <input type="radio" name="func" value="reverse" checked>Reverse string<br>
+        <input type="radio" name="func" value="palindrome">Check if the string is palindrome<br>
+        <input type="radio" name="func" value="encrypt">Encrypt string<br>
+        <input type="radio" name="func" value="decrypt">Decrypt string<br><br><br>
+        
+        Input string:&nbsp;
+        <input type="text" name="value" required>
+        <input type="submit" value="Execute" class="button">
+        <br><br>
+        <span>Result:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span><span id="result"></span>
+    </form>
+</div>
 @endsection
